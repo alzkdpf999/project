fc63857c876102c6ae1fb79d94c0265dfad429a8
alzkdpf999 <alzkdpf999@naver.com>
2022-05-02 20:33:29 +0900

    Initial Commit

---
+++ hci/hciApp.swift
@@ -0,0 +1,17 @@
+//
+//  hciApp.swift
+//  hci
+//
+//  Created by 정충효 on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct hciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
